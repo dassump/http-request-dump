5c53af4d1556dfcacdae835683d10c835401c982
Daniel Dias de Assumpção <dassump@gmail.com>
2022-12-12 07:39:57 -0300

    More simplified variable definitions Remove unnecessary code from makefile

--- main.go
+++ main.go
@@ -10,34 +10,28 @@ import (
 )
 
 var (
-	app         string = "http-request-dump"
-	version     string = "dev"
-	description string = "HTTP request dump server"
-	site        string = "https://github.com/dassump/http-request-dump"
-	info        string = "%s (%s)\n\n%s\n%s\n\n"
-	usage       string = "Usage of %s:\n"
+	app         = "http-request-dump"
+	version     = "unknown"
+	description = "HTTP request dump server"
+	site        = "https://github.com/dassump/http-request-dump"
 
-	listen         string
-	listen_key     = "listen"
-	listen_default = "0.0.0.0:8888"
-	listen_info    = "Server address and port"
+	listen = flag.String("listen", "0.0.0.0:8888", "Server address and port")
 )
 
 func init() {
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), info, app, version, description, site)
-		fmt.Fprintf(flag.CommandLine.Output(), usage, os.Args[0])
+		fmt.Fprintf(flag.CommandLine.Output(), "%s (%s)\n\n%s\n%s\n\n", app, version, description, site)
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 
-	flag.StringVar(&listen, listen_key, listen_default, listen_info)
 	flag.Parse()
 }
 
 func main() {
 	log.SetPrefix(">>> ")
-	log.Println("Listening on", listen)
-	log.Fatal(http.ListenAndServe(listen, http.HandlerFunc(
+	log.Println("Listening on", *listen)
+	log.Fatal(http.ListenAndServe(*listen, http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
 			dump, _ := httputil.DumpRequest(r, true)
 			log.Printf("Request from %s\n%s", r.RemoteAddr, dump)
